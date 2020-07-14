9e2e371b55c70f40b3417bd691953115ac737461
wbmatthews <billm@me.com>
2020-07-13 21:19:35 -0400

    Added liked post toggle

--- Birdui/Birdui/PostListView.swift
+++ Birdui/Birdui/PostListView.swift
@@ -12,27 +12,52 @@ struct PostListView: View {
   
   @ObservedObject var postViewModel: PostViewModel
   @State var showingNewPostView = false
+  @State var isLikedPostsOnly = false
   
   var body: some View {
-    VStack {
-      ZStack {
-        HStack {
-          LogoView()
-            .padding(.horizontal, 10)
-          Spacer()
-        }
-        Text("Home")
-          .font(.largeTitle)
+    VStack(alignment: .leading) {
+      
+      VStack(alignment: .leading) {
+        TitleBarView()
         
+        Button("Create New Post") {
+          self.showingNewPostView.toggle()
+        }
+        .sheet(isPresented: $showingNewPostView) {
+          NewPostView(postHandler: self.postViewModel)
+        }
+        
+        Toggle(isOn: $isLikedPostsOnly) {
+          Text("Liked Posts Only")
+        }
+        
+        Divider()
       }
-      Button("Create New Post") {
-        self.showingNewPostView.toggle()
-      }.sheet(isPresented: $showingNewPostView) {
-        NewPostView(postHandler: self.postViewModel)
+      .padding(.horizontal, 12)
+      
+      List {
+        
+        ForEach(postViewModel.posts.indices, id:\.self) { postIndex in
+          Group {
+            if !self.isLikedPostsOnly || self.postViewModel.posts[postIndex].isLiked {
+              PostView(post: self.$postViewModel.posts[postIndex])
+            }
+          }
+        }
       }
-      List(postViewModel.posts.indices) { postIndex in
-        PostView(post: self.$postViewModel.posts[postIndex])
+    }
+  }
+}
+
+struct TitleBarView: View {
+  var body: some View {
+    ZStack(alignment: .center) {
+      HStack {
+        LogoView()
+        Spacer()
       }
+      Text("Home")
+        .font(.largeTitle)
     }
   }
 }
